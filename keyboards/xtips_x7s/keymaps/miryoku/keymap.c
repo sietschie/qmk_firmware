44ae928e25a1febec7d0a089466bb28fd3722bb8
sietschie <sietschie@gmail.com>
2025-01-16 22:13:42 +0100

    fix: missing quote key

--- keyboards/xtips_x7s/keymaps/miryoku/keymap.c
+++ keyboards/xtips_x7s/keymaps/miryoku/keymap.c
@@ -12,7 +12,7 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
         KC_Q,         KC_W,         KC_E,         KC_R,         KC_T,          /*||*/  KC_Y,         KC_U,          KC_I,         KC_O,           KC_P,
-        LGUI_T(KC_A), LALT_T(KC_S), LCTL_T(KC_D), LSFT_T(KC_F), KC_G,          /*||*/  KC_H,         LSFT_T(KC_J),  LCTL_T(KC_K), LALT_T(KC_L),   LGUI_T(KC_SCLN),
+        LGUI_T(KC_A), LALT_T(KC_S), LCTL_T(KC_D), LSFT_T(KC_F), KC_G,          /*||*/  KC_H,         LSFT_T(KC_J),  LCTL_T(KC_K), LALT_T(KC_L),   LGUI_T(KC_QUOTE),
         KC_Z,         RALT_T(KC_X), KC_C,         KC_V,         KC_B,          /*||*/  KC_N,         KC_M,          KC_COMM,      RALT_T(KC_DOT), KC_SLSH,
                                     LT(3,KC_ESC), LT(1,KC_SPC), LT(2,KC_TAB),  /*||*/  LT(5,KC_ENT), LT(4,KC_BSPC), LT(6,KC_DEL)
     ),
